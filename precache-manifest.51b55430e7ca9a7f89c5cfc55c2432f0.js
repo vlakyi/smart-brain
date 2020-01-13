7ba3f5b98b58adaad970551e70bc4f8ac3d29dec
Vlad Kiyashko <kiyashko.vlad1@gmail.com>
2020-01-13 01:13:22 +0100

    Updates

---
+++ precache-manifest.51b55430e7ca9a7f89c5cfc55c2432f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32ad2260584d6e07cf5429eafb4a1082",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "ab758a79ddc63bc0b72c",
+    "url": "/smartbrain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "eaef09fe94f95810646f",
+    "url": "/smartbrain/static/css/main.f870de30.chunk.css"
+  },
+  {
+    "revision": "ab758a79ddc63bc0b72c",
+    "url": "/smartbrain/static/js/2.8f01dd0c.chunk.js"
+  },
+  {
+    "revision": "eaef09fe94f95810646f",
+    "url": "/smartbrain/static/js/main.0821f650.chunk.js"
+  },
+  {
+    "revision": "853246cea5739a537f3f",
+    "url": "/smartbrain/static/js/runtime-main.3e544847.js"
+  }
+]);
